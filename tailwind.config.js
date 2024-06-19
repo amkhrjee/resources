c27a4b98fc52f8b6b13c6550c373943d7f681142
amkhrjee <aniruddha.mukherji@outlook.com>
2024-06-19 17:48:43 +0530

    add: few more sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html"],
+  daisyUi: {
+    themes: ["black"],
+  },
   theme: {
     extend: {
       fontFamily: {
         sans: ["Spectral", "system-ui"],
+        serif: ["Inter Tight"],
       },
     },
   },
